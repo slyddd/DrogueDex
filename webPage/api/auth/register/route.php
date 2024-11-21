8a42f134703e8ce0cea90c15f53c5bdd020a40ce
SebasCastillo18 <sebascasti2019@gmail.com>
2024-11-20 19:33:24 -0500

    connect loginin route.php

--- webPage/api/auth/register/route.php
+++ webPage/api/auth/register/route.php
@@ -2,18 +2,19 @@
 require __DIR__ . '/../authController.php';
 
 header('Access-Control-Allow-Origin: *');
-header('access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
 
-$loginController = new AuthController();
+$authController = new AuthController();
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
     case 'POST':
-        $loginController->register(file_get_contents('php://input'));
+        $authController->register(file_get_contents('php://input'));
         break;
     default:
-        echo json_encode(['message' => ERRORS::_METHOD_NOT_SUPPORTED_]);
+        echo json_encode(['message' => 'Método no permitido']);
         break;
 }
+?>
